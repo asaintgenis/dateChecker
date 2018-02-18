f49bd40d2027ef25db2528e60ee672ea440bd7d9
Arthur Saint-Genis <a.saintgenis@gmail.com>
2018-02-18 18:22:31 +0100

    add spring actuator

--- src/main/java/com/asaintgenis/DateChecker/model/DateRequest.java
+++ src/main/java/com/asaintgenis/DateChecker/model/DateRequest.java
@@ -1,10 +1,15 @@
 package com.asaintgenis.DateChecker.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotEmpty;
 
+@ApiModel(description = "The request send a pattern and the date to parse")
 public class DateRequest {
 
     @NotEmpty
+    @ApiModelProperty(notes = "the pattern should be a valid java date format")
     String pattern;
 
     @NotEmpty
